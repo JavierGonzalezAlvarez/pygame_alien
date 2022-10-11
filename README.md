c5eb639383e26a7599a372b228f72dab5517724e
javier <jgalvarez.jga@gmail.com>
2022-10-11 09:07:24 +0200

    sound

---
+++ README.md
@@ -0,0 +1,6 @@
+## Alien game
+$ pip install pygame
+
+<= left arrow : move shooter to the left
+right arrow => : move shooter to the right
+up arrow : shoot
--- aliens.py
+++ aliens.py
@@ -11,6 +11,7 @@ pygame.init()
 pygame.mixer.init()
 # instance
 shootSound = pygame.mixer.Sound('shoot.wav')
+blastSound = pygame.mixer.Sound('blast.wav')
 
 # create an instance of Clock()
 '''
@@ -45,18 +46,25 @@ img_gun = pygame.transform.rotate(img_gun, 90)
 img_gun_rect = img_gun.get_rect()
 
 # text
-ping_text = "Score: "
+score = 0
+ping_text = "Score: " + str(score)
 font = pygame.font.SysFont(None, 25)
 txt_score = font.render(ping_text, True, (0, 0, 0))
 
+
+def shoot(pos_alien, pos_gun):
+    print(pos_alien, " - ", pos_gun)
+    if pos_alien == pos_gun or pos_alien + 1 == pos_gun or pos_alien - 1 == pos_gun:
+        blastSound.play()
+        print("you get it!")
+        score += 1
+
+
 x = 0
-pos = 20
+pos = 2
 while True:
     # fill the color on the surface
     surface.fill(background)
-
-    # show text
-    surface.blit(txt_score, (width - 200, height/2))
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -78,23 +86,25 @@ while True:
             if event.key == pygame.K_UP:
                 # play the sound
                 shootSound.play()
+                shoot(img_alien_rect.x, x)
                 # line(surface, color, start_pos, end_pos, width=1) -> Rect
                 pygame.draw.line(surface, shoot_color,
-                                 (60, 80), (60, 100), width=4)
+                                 (x, 450), (x, 470), width=4)
+
+    # show text
+    txt_score = font.render(ping_text, True, (0, 0, 0))
+    surface.blit(txt_score, (width - 200, height/2))
 
     # show img  & set position of the gun. first time and move img gun
     surface.blit(img_gun, (x, height - 120))
 
     # move img alien
-    img_alien_rect = img_alien_rect.move(2, 0)
+    img_alien_rect = img_alien_rect.move(1, 0)
     # blit() draw one image onto another position
     surface.blit(img_alien, img_alien_rect)
 
     # checking img inside the screen
     if img_alien_rect.left < 0 or img_alien_rect.right > width:
-        img_alien_rect.x = 0
-        img_alien_rect.y = 0
-    if img_alien_rect.top < 0 or img_alien_rect.bottom > height:
         img_alien_rect.x = 0
         img_alien_rect.y = 0
 
